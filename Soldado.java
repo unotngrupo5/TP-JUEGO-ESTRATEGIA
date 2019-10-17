f088b4803417a6b2c9caf5481809d80f8fe45445
unotngrupo5 <54962569+unotngrupo5@users.noreply.github.com>
2019-10-17 10:43:43 -0300

    Update Soldado.java

--- Soldado.java
+++ Soldado.java
@@ -14,45 +14,58 @@ public class Soldado extends Unidad{
 		//a.atack(b);
 		//a.atack(b);
 		System.out.println("b:"+b);
-		b.atack(a);		
-		System.out.println("a:"+a);
-		a.move("cnnnnof");
-		System.out.println("a:"+a);
+		b.atack(a);	
+		a.atack(b);
 		a.atack(b);
 		System.out.println("a:"+a);
+		a.move("cnnnnof");
+		System.out.println("a:"+a);		
+		b.atack(a);
+		System.out.println("a:"+a);
 		System.out.println("b:"+b);
+		a.pocionA();
+		System.out.println("a:"+a);
+		a.atack(a);
 	}
-	
-	private Integer ep;	
-	
+
+	private static Integer ep=30;
+	private static Integer hp=200;
+	private static final Integer p_atk=10;
+	private static final Integer DMIN=1;
+	private static final Integer DMAX=1;
+
 	public Soldado(Integer x,Integer y) {
-		super(x,y);
-		this.setHp(200);
-		this.setP_atk(10);
-		this.setEp(20);		
+		super(x,y,hp,p_atk,DMAX,DMIN);		
 	}
-	
+
 	public void atack(Unidad b) {
-		if(this.dead()==true) {
-			System.out.println("La unidad esta muerta");
-			return;
-		}
-		if (this.distance(b)==1) {
-			if(this.getEp()!=0) {
+		if(this.equals(b)) return;
+		if(this.dead()||b.dead())	return;
+		if (this.calcuDistance(b)) {
+			if(this.tengoEp()) {
 				b.setHp(b.getHp()-this.getP_atk());
-				this.setEp(this.getEp()-10);
-			}	
-			else System.out.println("I need more energy");
+				ep=this.getEp()-10;
+			}	 
 		}
-		else System.out.println("It out of Range");
 	}	
+
+	public void pocionA(){
+		ep=this.getEp()+10;
+	}
+
+	public boolean tengoEp() {
+		if(this.getEp()==0) {
+			System.out.println("I need more energy");
+			return false;
+		}
+		return true;
+	}
+
+	public void paquete() {
+	}
 
 	public Integer getEp() {
 		return ep;
-	}
-
-	public void setEp(Integer ep) {
-		this.ep = ep;
 	}
 
 	@Override
